359ec9e67300d87c52e259484a946ca3f5e1873c
allengblack <allengblack@gmail.com>
2017-09-06 15:14:02 +0100

    chore: create initial files

---
+++ AlertHandler/AlertHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.SessionState;
+
+namespace AlertHandler
+{
+    public class AlertHandler
+    {
+        private const string storageKey = "alert-notifications";
+        private static HttpContext Context
+        {
+            get
+            {
+                return HttpContext.Current;
+            }
+        }
+
+        private static HttpSessionState Session
+        {
+            get
+            {
+                return Context.Session;
+            }
+        }
+
+        private static List<AlertNotification> Notifications
+        {
+            get
+            {
+                if (Session[storageKey] == null)
+                {
+                    Session[storageKey] = new List<AlertNotification>();
+                }
+                return (List<AlertNotification>)Session[storageKey];
+            }
+        }
+
+        public static void AddNotification(AlertNotification notification)
+        {
+            Notifications.Add(notification);
+        }
+
+        public static string RenderNotifications()
+        {
+            string ret = "<script>\n" +
+                            "$(document).ready(function () {\n" +
+                            String.Join("\n", Notifications.Select(notification => "app.notifyMe({ message: '" + notification.Message + "', type: '" + notification.Type.ToString().ToLower() + "' });")) +
+                            "});\n" +
+                        "</script>";
+            Notifications.Clear();
+            return ret;
+        }
+
+        public static string RenderNotifications(Func<List<AlertNotification>, string> templateFn)
+        {
+            string ret = templateFn(Notifications);
+            Notifications.Clear();
+            return ret;
+        }
+    }
+}
---
+++ AlertHandler/AlertNotification.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertHandler
+{
+    public class AlertNotification
+    {
+        public string Message { get; set; }
+        public NotificationType Type { get; set; }
+
+        public enum NotificationType
+        {
+            Error,
+            Success,
+            Warning
+        }
+
+        public static AlertNotification Success(string message)
+        {
+            return new AlertNotification()
+            {
+                Type = NotificationType.Success,
+                Message = message
+            };
+        }
+
+        public static AlertNotification Warning(string message)
+        {
+            return new AlertNotification()
+            {
+                Type = NotificationType.Warning,
+                Message = message
+            };
+        }
+
+        public static AlertNotification Error(string message)
+        {
+            return new AlertNotification()
+            {
+                Type = NotificationType.Error,
+                Message = message
+            };
+        }
+    }
+}
